dfd0ded1726bfd6d08fb0c7e371055a8baa43b6a
Deng <bbfre.deng@sap.com>
2022-06-22 11:02:13 +0800

    fix auto push

--- plugin/hook/index.js
+++ plugin/hook/index.js
@@ -3,10 +3,10 @@ const port = 9988;
 const app = express();
 
 app.listen(port, () => {
-    console.log(`running at http://127.0.0.1:${port}`);
+  console.log(`running at http://127.0.0.1:${port}`);
 })
 
 app.post('/githook', (req, res) => {
-    console.log(req)
-    res.send('hello world')
+  console.log(req)
+  res.send('hello world')
 })
