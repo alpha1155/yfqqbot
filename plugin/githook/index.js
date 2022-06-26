4070a0f187fefc2623f70541f38c2bb8bd583a0f
BBFre <2931470156@qq.com>
2022-06-26 12:40:02 +0800

    translate avoid emoji

---
+++ plugin/githook/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// const ws = require('../bot/ws');
+const port = 9988;
+const app = express();
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`running at http://127.0.0.1:${port}`);
+})
+
+app.post('/githook', (req, res) => {
+    ws.send('send_private_msg', {
+        user_id: 2931470156,
+        message: req
+    })
+    console.log(req)
+    res.send('hello world')
+})
