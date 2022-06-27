87834d93659aba0b223d9d638cab6453d3fc17e5
Deng <bbfre.deng@sap.com>
2022-06-27 19:21:53 +0800

    beautify qrcode

--- plugin/qrcode/service.js
+++ plugin/qrcode/service.js
@@ -1,76 +1,79 @@
-const fs = require("fs");
-const path = require("path");
-const os = require("os");
-const axios = require("axios");
-const { url } = require("inspector");
-const sharp = require("sharp");
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+const axios = require('axios')
+const { url } = require('inspector')
+const sharp = require('sharp')
 
 async function getImage (text) {
   return new Promise(async (resolve, reject) => {
     const filename = path.join(
       // os.tmpdir(),
       `go_cqhttp_node_qrcode_${Date.now()}.svg`
-    );
+    )
     const filenamePng = path.join(
-      // os.tmpdir(),
+      os.tmpdir(),
       `go_cqhttp_node_qrcode_${Date.now()}.png`
-    );
-    const urlGetQrcode = "http://api.qrbtf.com/qrcode";
+    )
+    const urlGetQrcode = 'http://api.qrbtf.com/qrcode'
     await axios({
-      method: "GET",
+      method: 'GET',
       url: urlGetQrcode,
       params: {
         data: text,
-        level: "M",
-        style: "base",
-        type: "round",
+        level: 'M',
+        style: 'base',
+        type: 'round',
         size: 50,
         opacity: 30,
-        posType: "planet",
-        otherColor: "%23000000",
-        posColor: "%23000000",
+        posType: 'planet',
+        otherColor: '%23000000',
+        posColor: '%23000000',
       },
-    }).then((res) => {
+    }).then(res => {
       // console.log(res.data)
-      fs.writeFile(filename, res.data, (err) => {
+      fs.writeFile(filename, res.data, err => {
         if (err) {
-          console.error(err);
-          return;
+          console.error(err)
+          return
         }
         //文件写入成功。
-      });
-    });
+      })
+    })
     // res.body.pipe(stream)
     console.log(filename)
-    await sharp(filename, { animated: true })
+    const image = await sharp(filename, { animated: true })
+    const metadata = await image.metadata();
+    console.log(metadata)
+    await image
       .png()
       .toFormat('png')
       .toFile(filenamePng)
-      .then((res) => {
-        console.log(res);
+      .then(res => {
+        console.log(res)
       })
-      .catch((err) => {
-        console.error("[qrcode]", err);
+      .catch(err => {
+        console.error('[qrcode]', err)
         resolve([
           {
-            type: "text",
+            type: 'text',
             data: {
-              text: "生成二维码失败",
+              text: '生成二维码失败',
             },
           },
-        ]);
-      });
+        ])
+      })
     resolve([
       {
-        type: "image",
+        type: 'image',
         data: {
-          file: "file://" + filenamePng,
+          file: 'file://' + filenamePng,
         },
       },
-    ]);
-  });
+    ])
+  })
 }
 
 module.exports = {
   getImage,
-};
+}
