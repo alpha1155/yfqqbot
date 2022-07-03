a4e2569672de4823b7b0514c4c3915e7b5e48593
BBFre <2931470156@qq.com>
2022-07-03 16:46:18 +0800

    reshou

--- plugin/whatIsHot/service.js
+++ plugin/whatIsHot/service.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 
 
-async function getZhihuHot () {
+async function getZhihuHot() {
   try {
     let url =
       "https://tenapi.cn/zhihuresou/";
@@ -37,7 +37,7 @@ async function getZhihuHot () {
     ]
   }
 }
-async function getDouyinHot () {
+async function getDouyinHot() {
   try {
     let url =
       "https://tenapi.cn/douyinresou/";
@@ -67,6 +67,41 @@ async function getDouyinHot () {
         type: 'text',
         data: {
           text: '抖音找不到'
+        }
+      }
+    ]
+  }
+}
+async function getWeiboHot() {
+  try {
+    let url =
+      "https://tenapi.cn/douyinresou/";
+    let message = await axios({ url: url, method: "get" }).then((res) => {
+      console.log(res.data.list);
+      let data = res.data.list;
+      let msg = '微博热搜：'
+      for (let item of data) {
+        msg += '\n' + item.name;
+      }
+      return msg;
+    });
+
+
+    return [
+      {
+        type: 'text',
+        data: {
+          text: message
+        }
+      }
+    ]
+  } catch (e) {
+    console.error('[weibo]', e)
+    return [
+      {
+        type: 'text',
+        data: {
+          text: '微博走丢了'
         }
       }
     ]
